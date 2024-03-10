abb4f9c40c25d4ee22b7034976190d570fc559f3
Mamya22 <3284727604@qq.com>
2024-03-10 20:59:16 +0800

    debug

--- lab0/bubblesort.cpp
+++ lab0/bubblesort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+
 void bubbleSort(std::vector<int>& arr) {
     int n = arr.size();
     for (int i = 0; i < n - 1; ++i) {
@@ -8,6 +9,10 @@ void bubbleSort(std::vector<int>& arr) {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
             }
+    	 #ifdef DEBUG  
+    	 for (int num : arr) std::cout << num << " ";
+    	 std::cout << std::endl;
+	 #endif       
         }
     }
 }
