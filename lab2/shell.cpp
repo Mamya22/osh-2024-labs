be5f647688d438e77f8d6d9127152bb3df50845e
Mamya22 <3284727604@qq.com>
2024-04-20 13:15:05 +0800

    pipe

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -12,6 +12,82 @@
 #include <unistd.h>
 // wait
 #include <sys/wait.h>
+std::vector<std::string> split(std::string s, const std::string &delimiter);
+int BuildPipeCmd(std::string &cmd){
+	std::vector<std::string> pipe_cmd = split(cmd," | ");
+	if(pipe_cmd.empty()){
+		return 0;
+	}
+	size_t pipe_num = pipe_cmd.size();
+	if(pipe_num == 1){
+
+	}
+	// 需要创建 num - 1 个 fd，管道的内容需要传递，所以需要一个中间量,保存每次读端口，方便下一次使用
+	else{
+		int mid_bridge;
+		for(size_t i = 0; i < pipe_num; i++){
+			int fd[2];
+			pipe(fd);
+			pid_t pid = fork();
+			if(pid < 0){
+				std::cout << "Error pipe: can not create pipe" << std::endl;
+				return 3;
+			}
+			// 对子进程
+			if(pid  == 0){
+				if(i == 0){
+					close(fd[0]);
+					dup2(fd[1], STDOUT_FILENO); //第一个子进程传入数据
+				    close(fd[1]);
+				}
+				else if(i == pipe_num - 1){
+					close(fd[1]);
+					dup2(mid_bridge, STDIN_FILENO);
+					close(mid_bridge);
+				}
+				else{
+					dup2(mid_bridge, STDIN_FILENO); //其余子进程读入上一个
+					dup2(fd[1], STDOUT_FILENO);
+					close(fd[0]);
+					close(fd[1]);
+					close(mid_bridge);
+				}
+			}
+			// 对父进程
+			else{
+				mid_bridge = fd[0];
+				close(fd[1]);
+				close(fd[0]);
+
+			}
+		}
+		int status;
+		while(waitpid(-1,&status, WNOHANG) != -1){
+			continue;
+		}
+		 
+	}
+	/*
+	else if(pipe_num == 2){
+		int fd[2];
+		int ret = pipe(fd);
+		if(ret == -1){
+			std::cout <<"Error pipe: can not create pipe"<<std::endl;
+			return 4;
+		}
+		pid_t pid = fork();
+		if(pid == 0){
+			close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+
+		}
+		else if(pid > 0){
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+		}
+	}
+	*/
+}
 
 int BuildInnerCmd(std::vector<std::string> &args){
 	if(args.empty()){
@@ -54,28 +130,28 @@ int BuildInnerCmd(std::vector<std::string> &args){
 				}
 				else if(args[1][0] == '$'){
 					size_t index = args[1].find_first_of('/');
-					std::string envirPath;
+					char* envirPath;
 					if(index == std::string::npos){
-						if(args[1].size() == 1){
+						if(args[1].length() == 1){
 							std::cout << "Error cd: Failed to get environment path" << std::endl;
 							return 3;
 						}
 						else{
-		                    envirPath = getenv(args[1].substr(1, args[1].length()-1).c_str());
-							if(envirPath.empty()){
+		                   	envirPath = getenv(args[1].substr(1, args[1].length()-1).c_str());
+							if(envirPath==NULL){
 								std::cout << "Error cd: Failed to get environment path" << std::endl;
 								return 3;
 							}
-	                        cd_ret = chdir(envirPath.c_str());
+	                        cd_ret = chdir(envirPath);
 						}
 					}
 					else {
 						envirPath = getenv(args[1].substr(1,index).c_str());
-	                        if(envirPath.empty()){
+	                        if(envirPath==NULL){
 								std::cout << "Error cd: Failed to get environment path" << std::endl;
                                 return 3;
 							}
-						cd_ret = chdir(envirPath.c_str());
+						cd_ret = chdir(envirPath);
 					}
 				}
 				else
@@ -91,7 +167,7 @@ int BuildInnerCmd(std::vector<std::string> &args){
 	}
 	return 0;
 }
- std::vector<std::string> split(std::string s, const std::string &delimiter);
+
 int main(){
 	std::ios::sync_with_stdio(false);
 	std::string cmd;
